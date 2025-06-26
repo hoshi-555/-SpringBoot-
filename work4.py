4e0885a57c4508324385e910e6de9e4c4e5ee5da
hoshi-555 <3179748230@qq.com>
2025-06-26 11:31:41 +0800

    work4.py

--- work4.py
+++ work4.py
@@ -38,7 +38,7 @@ def get_answer(question: str, strict_file_mode: bool = False):
             model='deepseek-reasoner',
             messages=messages,
             temperature=0,
-            max_tokens=1024
+            max_tokens=2048
         )
 
         return response.choices[0].message.content
